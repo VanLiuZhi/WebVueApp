8ce4f57d2c17a9a688d1ab32f716b103248064cf
liuzhi <1441765847@qq.com>
2018-09-03 10:30:18 +0800

    upgrade url 配置调整

--- vanliuweb/urls.py
+++ vanliuweb/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # user:vanliu  password:destinymy3
@@ -25,3 +26,5 @@ urlpatterns = [
     path('vadmin/', (include('vadmin.views'))),
     path('user/', include('user.views'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
